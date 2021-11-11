ec2ec883abfd91baf97e4c41092a933b7888c20e
Tigran <tigran.arsenyan.2015@gmiail.com>
2021-11-11 23:04:02 +0400

    Initial Commit

---
+++ ruler-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ruler-ios
+//
+//  Created by Tigran Arsenyan on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
